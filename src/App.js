552fc6733360f6d141fac23434fe4380b14f061a
Kin <winterheat@gmail.com>
2017-04-18 00:46:05 -0700

    add another method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TogglePanel from "./components/toggle-panel";
+import ToggleExistentialPanel from "./components/toggle-existential-panel";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <TogglePanel isOpen={true} />
+        <ToggleExistentialPanel isOpen={true} />
       </div>
     );
   }
--- src/components/toggle-existential-panel.js
+++ src/components/toggle-existential-panel.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 // <TogglePanel isOpen={false} />
 // this.props.isOpen
-export default class TogglePanel extends Component {
+export default class ToggleExistentialPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {open: props.isOpen};
@@ -20,7 +20,7 @@ export default class TogglePanel extends Component {
     return (
       <div className="toggle-panel">
         <button onClick={() => { this.toggle() }}>Click</button>
-        <div className={(this.state.open ? "" : "hidden") + " panel"}>panel content<div>Random Number: { Math.floor(Math.random() * 10000) }</div></div>
+        {this.state.open && <div className="panel">panel content<div>Random Number: { Math.floor(Math.random() * 10000) }</div></div>}
       </div>
     );
   }
